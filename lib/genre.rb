16b972d4f726068667fd7c00808bf33d778a8acc
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-25 15:18:02 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,13 +1,20 @@
-class Genre 
+class Genre
   attr_accessor :name
-  @@all=[]
-def initialize(name)
-  @name=name
-  @@all << self 
-end
-def self.all 
-@@all
-end
-  
-  
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    Song.all{|song| song.genre == self}
+  end
+
+  def artists
+    songs.map{|song| song.artist}
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
 end
