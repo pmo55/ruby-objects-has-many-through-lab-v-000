96abc378c6abd6b451f52895b73bedb63711cfb0
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-25 15:13:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,12 @@
 class Song
   @@all=[]
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@all << self
+    
+  end
   
 end
